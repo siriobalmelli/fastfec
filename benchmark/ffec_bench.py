1ae29199d6da46fbd5033c9c13763a0c7a04b314
Sirio Balmelli <sirio.bm@gmail.com>
2017-07-13 12:12:20 -0400

    proper file naming

--- benchmark/ffec_bench.py
+++ benchmark/ffec_bench.py
@@ -90,7 +90,7 @@ def gen_benchmark():
     # test across an exponential range of sizes
     ret['symbol_sz'] = 1280
     ret['Y_size'] = [ 2**sz_exp * ret['symbol_sz']
-                for sz_exp in range(8, 20) ] #20
+                for sz_exp in range(8, 21) ] #21
 
     # Generate mesh of graphing coordinates against which
     #+  to run tests (so they execute in the right order!)
@@ -117,10 +117,60 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
 from matplotlib import cm
 
-def format_plot(ax):
-    '''common plot formatting code in one place.
-    ax : a subplot.
-    '''
+#from matplotlib.colors import LogNorm
+
+def make_plots_alt(bm):
+    lin_x = bm['X_ratio'] # for legibility only
+    log_y = [ log(y, 2) for y in bm['Y_size'] ]
+    lin_y = [ y for y in bm['Y_size'] ] # used for labels
+    X, Y = np.meshgrid(lin_x, log_y)
+
+    # Repeat plotting work for the following:
+    do_plots = { "Z_inef" : ("Inefficiency", "inefficiency"),
+                "Z_enc" : ("Encode Bitrate (Gb/s)", "encode" ),
+                "Z_dec" : ("Decode Bitrate (Gb/s)", "decode" )
+                }
+
+    for k, v in do_plots.items():
+        lin_z = bm[k]
+        Z = np.array(lin_z).reshape(X.shape) 
+
+        _, ax = plt.subplots()
+        p = plt.pcolormesh(X, Y, Z, cmap=cm.get_cmap('Vega20'), axes=ax,
+                            vmin=bm[k][0], vmax=bm[k][-1])
+
+        ax.set_xlabel('ratio')
+        ax.set_xticks(lin_x, minor=False)
+        ax.set_ylabel('block size (log2)')
+        ax.set_yticks(log_y, minor=False)
+        ax.set_yticklabels(lin_y)
+
+        # standard label formatting
+        ax.tick_params(labelsize = 5)
+        ax.set_frame_on(False)
+        ax.grid(True)
+
+        # colorbar
+        cb = plt.colorbar(p, ax=ax, orientation='horizontal', aspect=20)
+        cb.ax.set_xlabel(v[0])
+        # generate precisely 20 tick values, so they will fit on the borders
+        #+  of the 20 colormap squares
+        step = (lin_z[-1] - lin_z[0]) / 20
+        ticks = [ lin_z[0] + step * i for i in range(20) ]
+        cb.set_ticks(ticks)
+        # truncate the tick label to 5 characters without affecting the values
+        cb.set_ticklabels([ '%.5s' % z for z in ticks ])
+        # standard label formatting
+        cb.ax.tick_params(labelsize = 5)
+        cb.ax.set_frame_on(False)
+        cb.ax.grid(True)
+
+        # layout; save
+        plt.title('ffec: [commit]'.format(v[0]))
+        plt.tight_layout()
+        plt.savefig('{0}.pdf'.format(v[1]), dpi=600,
+                    papertype='a4', orientation='landscape')
+
 
 def make_plots(bm):
     '''take a benchmarks dictionary 'bm' and write plots to disk
@@ -138,32 +188,68 @@ def make_plots(bm):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         Z = np.array(bm[k]).reshape(X.shape) 
-        ax.plot_surface(X, Y, Z, cmap=cm.gist_ncar, antialiased=True, linewidth=0, shade=False)
+        ax.plot_surface(X, Y, Z, cmap=cm.gist_ncar, antialiased=False, linewidth=0, shade=True)
+
+        #ticks
+        ax.set_xlabel('ratio')
+        ax.set_xticks(bm['X_ratio'], minor=True)
+        ax.minorticks_on()
+        ax.invert_xaxis()
+#        ax.tick_params(axis='x',which='minor',bottom='off')
+        ax.set_ylabel('block size (log2)')
+        ax.invert_yaxis()
+#        ax.axis(v = 'tight')
+
         # labels
         ax.tick_params(labelsize = 6)
         ax.set_frame_on(False)
         ax.grid(True)
-#        ax.axis(v = 'tight', xmin = bm['X_ratio'][0], xmax = bm['X_ratio'][-1],
-#                ymin = log(bm['Y_size'][0], 2), ymax = log(bm['Y_size'][-1], 2))
-        ax.set_xlabel('Ratios')
-        ax.invert_xaxis()
-        ax.set_ylabel('Sizes (log2)')
-        ax.invert_yaxis()
+
         # layout; save
-        plt.title('ffec; {0}; [commit]'.format(v[0]))
-        plt.tight_layout()
+        plt.title('ffec: {0}; [commit]'.format(v[0]))
+#        plt.tight_layout()
         plt.savefig('{0}.pdf'.format(v[1]), dpi=600,
                     papertype='letter', orientation='landscape')
 
 
 
+def current_commit():
+    '''returns the current git commit; or dies
+    '''
+    # get current git commit; ergo must run inside repo
+    try:
+        #TODO: This is hardcoded. That is bad.
+        sub = subprocess.run([ 'git', 'rev-parse', '--short', 'HEAD' ],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            shell=False, check=True);
+    except subprocess.CalledProcessError as err:
+        print(err.cmd)
+        print(err.stdout.decode('ascii'))
+        print(err.stderr.decode('ascii'))
+        exit(1)
+
+    return sub.stdout.decode('ascii')
+
+
+
+import sys
 import os
 import json
+import socket
+from datetime import datetime
 
 if __name__ == "__main__":
 
+    # allow caller to specify the filename, otherwise assume 'benchmark.json'
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = '{0}.{1}.{2}.bench.json'.format(
+                datetime.now().date(),
+                current_commit(),
+                socket.gethostname())
+
     # look for an existing benchmark.json; if exists load that one
-    filename = 'benchmark.json'
     if os.path.isfile(filename):
         with open(filename) as f:
             bm = json.load(f)
@@ -173,4 +259,5 @@ if __name__ == "__main__":
         with open(filename, 'w') as f:
             json.dump(bm, f)
 
-    make_plots(bm)
+#    make_plots(bm)
+    make_plots_alt(bm)
