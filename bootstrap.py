a5edf327e1f98ef6631c524a0ec5d9a0a9c48863
alexlab2017 <30337560+alexlab2017@users.noreply.github.com>
2017-08-22 22:05:42 +0200

    Main change: reviewed helper functions
    - substituted 'run_die', 'run_output', 'run_shell', 'run_return' with 'run' and different kwargs
    - removed 'pushd' and used 'subprocess.run(cwd=path\here)'
    - removed platform module and used sys module instead
    - changed some variable's names to be consistent
    - changed string formatting to more recent style

--- bootstrap.py
+++ bootstrap.py
@@ -1,153 +1,136 @@
 #!/usr/bin/env python3
 
 import subprocess
-import platform
 import sys
 import os
 
+def run(command_seq_, shell_=False, die_=False, output_=False, cwd_=None):
 
-def run_die(command_seq_, shell_=False):
+    if shell_:
+        command_seq_=' '.join(command_seq_)
 
-    print('\n\nEXEC: %s' % ' '.join(command_seq_))
+    proc = subprocess.run(command_seq_,
+                          stderr=subprocess.STDOUT,
+                          stdout=subprocess.PIPE,
+                          shell=shell_,
+                          cwd=cwd_,
+                          )
 
-    if shell_ == True:
-        proc = subprocess.Popen(' '.join(command_seq_), shell=True)
+    if die_ and proc.returncode:
+        if not shell_:
+            command_seq_=' '.join(command_seq_)
+                    print('{0}\n\nfailed: {1}'.format(proc.stdout, command_seq_),
+              file=sys.stderr,
+              )
+        exit(proc.returncode)
+
+    if output_:
+        return proc.stdout
     else:
-        proc = subprocess.Popen(command_seq_)
-        
-    proc.wait()
-    
-    if proc.returncode:
-        print('failed: %s' % ' '.join(command_seq_), file=sys.stderr)
-        exit(1)
-    else:
-        return 0
-
-
-def run_return(command_seq_, shell_=False):
-
-    if shell_ == True:
-        proc = subprocess.Popen(' '.join(command_seq_), shell=True)
-    else:
-        proc = subprocess.Popen(command_seq_)
-        
-    proc.wait()
-    
-    return proc.returncode
-
-
-def run_output(command_seq_):
-
-    proc = subprocess.Popen(command_seq_,
-                            stderr=subprocess.STDOUT,
-                            stdout=subprocess.PIPE,
-                            )
-    stdout_, stderr_ = proc.communicate()
-    return stdout_.decode('utf-8').strip()
-
-
-def pushd(desired_dir_):
-    current_ = os.getcwd()
-    os.chdir(desired_dir_)
-    return current_
+        return proc.returncode
 
 
 def comp_ver(exist_, required_, name_):
-    
+
     if exist_ >= required_:
         return 0
     else:
-        print("'%s'version '%s' does not meet required '%s'" % (name_, exist_, required_), file=sys.stderr)
+        print("'{0}'version '{1}' does not meet required '{2}'".format(name_, exist_, required_),
+              file=sys.stderr,
+              )
         return 1
 
 
 def run_pkg(sudo_, mgr_, opt_, pkg_):
 
-    if platform.system() == 'win32':
+    if sys.platform() == 'win32':
         return 1
 
     for i in range(len(mgr_)):
-        if not run_return(['which', mgr_[i]]):
-            return run_return([sudo_[i], mgr_[i], opt_[i], pkg_[i]], shell_=True)
+        if not run(['which', mgr_[i]]):
+            return run([sudo_[i], mgr_[i], opt_[i], pkg_[i]], shell_=True)
 
-    print('failed to find a package manager and install %s' % pkg_[0], file=sys.stderr)
+    print('failed to find a package manager and install {0}'.format(pkg_[0]),
+          file=sys.stderr,
+          )
     return 1
 
 
 def check_ninja():
 
-    if run_return(['which', 'ninja']):
+    if run(['which', 'ninja']):
 
-        sudo_ = ["sudo",   "sudo",       "sudo",    "sudo",   "sudo",    "",        "sudo" ]
-        mgr_ = [ "pacman", "apt-get",    "dnf",     "emerge", "port",    "brew",    "pkg" ]
-        opt_ = [ "-S",     "-y install", "install", "",       "install", "install", "install" ]
-        pkg_ = [ "ninja",
+        SUDO = ["sudo",   "sudo",       "sudo",    "sudo",   "sudo",    "",        "sudo" ]
+        MGR = [ "pacman", "apt-get",    "dnf",     "emerge", "port",    "brew",    "pkg" ]
+        OPT = [ "-S",     "-y install", "install", "",       "install", "install", "install" ]
+        PKG = [ "ninja",
                 "ninja-build",
                 "ninja-build",
                 "dev-util/ninja",
                 "ninja",
                 "ninja",
                 "ninja" ]
-        run_pkg(sudo_, mgr_, opt_, pkg_)
+        run_pkg(SUDO, MGR, OPT, PKG)
 
     version_ = '1.7.2'
 
-    if run_return(['which', 'ninja']) or comp_ver(run_output(['ninja', '--version']), version_, 'ninja'):
+    if run(['which', 'ninja']) or comp_ver(run(['ninja', '--version'], output_=True), version_, 'ninja'):
 
         print('will try to download ninja binary...')
 
         url_ = "https://github.com/ninja-build/ninja/releases"
-        platform_ = platform.system().lower()
 
-        if platform_ == 'linux':
-            url_ = url_ + '/download/v%s/ninja-linux.zip' % version_
-        elif platform_ == 'darwin':
-            url_ = url_ + '/download/v%s/ninja-mac.zip' % version_
+        if sys.platform.startswith('linux'):
+            url_ = url_ + '/download/v{0}/ninja-linux.zip'.format(version_)
+        elif sys.platform.startswith('darwin'):
+            url_ = url_ + '/download/v{0}/ninja-mac.zip'.format(version_)
         else:
-            print("don't know how to handle '%s'" % platform_)
+            print("don't know how to handle '{0}'".format(sys.platform))
             exit(1)
 
-        run_die(['mkdir', '-p', './toolchain'])
-        run_die(['wget', '-O', './toolchain/ninja.zip', url_])
+        run(['mkdir', '-p', './toolchain'], die_=True)
+        run(['wget', '-O', './toolchain/ninja.zip', url_], die_=True)
         install_ = '/usr/local/bin'
-        run_die(['sudo', 'unzip', '-o', '-d', install_ + '/', './toolchain/ninja.zip'])
-        run_die(['sudo', 'chmod', 'go+rx', install_ + '/ninja'])
+        run(['sudo', 'unzip', '-o', '-d', install_ + '/', './toolchain/ninja.zip'], die_=True)
+        run(['sudo', 'chmod', 'go+rx', install_ + '/ninja'], die_=True)
 
         if comp_ver(run_output(['ninja', '--version']), version_, 'ninja'):
-            print("installed a binary ninja v%s to %s; 'ninja --version' still fails." % (version_, install_), file=sys.stderr)
-            print('Please check $PATH', file=sys.stderr)
+            print("installed a binary ninja v{0} to {1}; 'ninja --version' still fails.\n\
+                  Please check $PATH".format(version_, install_),
+                  file=sys.stderr,
+                  )
             exit(1)
 
 
 def check_meson():
-    
-    if run_return(['which', 'meson']):
 
-        if run_return(['which', 'pip3']):
+    if run(['which', 'meson']):
 
-            sudo_ = ["sudo",        "sudo",     "" ]
-            mgr_ = [ "apt-get",     "port",     "brew" ]
-            opt_ = [ "-y install",  "install",  "install" ]
-            pkg_ = [ "python3-pip", "py35-pip", "python3" ]
+        if run(['which', 'pip3']):
 
-            if run_pkg(sudo_, mgr_, opt_, pkg_):
+            SUDO = ["sudo",        "sudo",     "" ]
+            MGR = [ "apt-get",     "port",     "brew" ]
+            OPT = [ "-y install",  "install",  "install" ]
+            PKG = [ "python3-pip", "py35-pip", "python3" ]
+
+            if run_pkg(SUDO, MGR, OPT, PKG):
                 exit(1)
 
-        run_die(['sudo', '-H', run_output(['which', 'pip3']), 'install', 'meson'])
+        run(['sudo', '-H', run(['which', 'pip3'], output_=True), 'install', 'meson'], die_=True)
 
 
 
 def main():
 
-    if not run_return(['which', 'cscope']):
-        run_die(['cscope', '-b', '-q', '-U', '-I.include', '-s.src', '-s.test'])
+    if not run(['which', 'cscope']):
+        run(['cscope', '-b', '-q', '-U', '-I.include', '-s.src', '-s.test'], die_=True)
 
     check_ninja()
 
     check_meson()
 
-    run_die(['rm', '-rfv', 'build*'])
-    
+    run(['rm', '-rfv', 'build*'], die_=True)
+
     BUILD_NAMES = [ "debug", "debug-opt",      "release", "plain", "tsan",                "asan" ]
     BUILD_TYPES = [ "debug", "debugoptimized", "release", "plain", "debugoptimized",      "debugoptimized" ]
     BUILD_OPTS = [  "",      "",               "",        "",      "-Db_sanitize=thread", "-Db_sanitize=address" ]
@@ -155,25 +138,27 @@ def main():
     BUILD_TRAVIS = ["",      "yes",            "yes",     "yes",   "",                    "" ]
 
     for i in range(len(BUILD_NAMES)):
-        
-        if run_return(['$TRAVIS'], shell_=True) and BUILD_TRAVIS[i]:
+
+        if run(['$TRAVIS'], shell_=True) and BUILD_TRAVIS[i]:
             continue
 
-        run_return(['meson', BUILD_OPTS[i], '--buildtype', BUILD_TYPES[i], 'build-%s' % BUILD_NAMES[i]], shell_=True)
+        run(['meson', BUILD_OPTS[i],
+             '--buildtype', BUILD_TYPES[i],
+             'build-{0}'.format(BUILD_NAMES[i]),
+             ], shell_=True)
 
-        cwd_ = pushd('build-%s' % BUILD_NAMES[i])
+        run_dir_ = os.path.join(os.getcwd(), 'build-{0}'.format(BUILD_NAMES[i]))
 
-        if BUILD_GRIND[i] and not run_return(['which', 'valgrind']):
-            #shuld run_die() instead?
-            run_return(['VALGRIND=1', 'mesontest', "--wrap=\'valgrind --leak-check=full\'"], shell_=True)
+        if BUILD_GRIND[i] and not run(['which', 'valgrind']):
+            #shuld run and die? I suppose no.
+            run(['VALGRIND=1', 'mesontest', "--wrap=\'valgrind --leak-check=full\'"],
+                shell_=True,
+                cwd_=run_dir_,
+                )
 
-        os.chdir(cwd_)
 
 main()
-        
 
-    
 
 # TODO : add comments
 # TODO : add Windows support
-
