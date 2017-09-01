ffbb2debcec63cac2b60e4ef5481b91b75ec7649
tony <tony@b-ad.ch>
2017-09-01 20:27:09 +0100

    fix lack of file diff and check for output

--- util/test_ncp.py
+++ util/test_ncp.py
@@ -6,6 +6,8 @@
 import subprocess
 import sys
 import os
+import filecmp
+import collections as col
 
 ncp = 'util/ncp' 
 test_file = 'copy.bin'
@@ -23,41 +25,55 @@ def ncp_copy(cmds, ncp):
 	'''copy a file using cmds"'''
 	try:
 		cmds.insert(0, ncp)
-		subprocess.run(cmds,
+		sub = subprocess.run(cmds,
 				stdout=subprocess.PIPE, stderr=subprocess.PIPE,
 				shell=False, check=True);
+	
+		return sub.stdout.decode('ascii'), sub.stderr.decode('ascii')
 	except subprocess.CalledProcessError as err:
 		print(err.cmd)
 		print(err.stdout.decode('ascii'))
 		print(err.stderr.decode('ascii'))
 		exit(1)
 
-def check_output_files():
-	if os.path.isfile('no_flags.copy') and os.path.isfile('force.copy') \
-		and os.path.isfile('verbose.copy') and os.path.isfile('verbose_force.copy'):
-			os.remove('no_flags.copy')
-			os.remove('verbose.copy')
-			os.remove('force.copy')
-			os.remove('verbose_force.copy')
-			os.remove(test_file)
-			exit(0)
-	else:
-		exit(1)
+output_files = [ 'no_flags.copy', 'verbose.copy' ]
+# No output is valid, so we add it to the list.
+verbose_output = [ '', '\'copy.bin\' -> \'verbose.copy\'' ]
 
-cmds = { 'clean'	: [ test_file, 'no_flags.copy'],
-		 'force'	: [ '-f', test_file, 'force.copy'],
-		 'verbose'	: [ '-v', test_file, 'verbose.copy'],
-		 'force_verbose' : [ '-v', '-f', test_file, 'verbose_force.copy']
-		 }
+def check_output_files():
+	for out_file in output_files:
+		if not os.path.isfile(out_file):
+			exit(1)
+		else:
+			if not filecmp.cmp(test_file, out_file):
+				exit(1)
+			else:
+				os.remove(out_file)
+
+	os.remove(test_file)
+
+# Use OrderedDict such that iterations are always in the order of elements inserted.
+# To make sure that 'force' tests always succeed overwriting previously created file.
+cmds = col.OrderedDict([
+		 ('clean', [ test_file, output_files[0]]),
+		 ('force', [ '-f', test_file, output_files[0]]),
+		 ('verbose', [ '-v', test_file, output_files[1]]),
+		 ('force_verbose', [ '-v', '-f', test_file, output_files[1]])
+		 ])
 
 #   main()
 if __name__ == "__main__":
 	if sys.argv[1] is not None:
 		ncp = sys.argv[1]
-
+	
 	create_random_file()
 
 	for k,v in cmds.items():
-		ncp_copy(v, ncp)
+		stdout, stderr = ncp_copy(v, ncp)
+		# Check output, if any
+		if stdout.rstrip() not in verbose_output:
+			exit(1)
+		elif stderr:
+			exit(1)
 
 	check_output_files()
